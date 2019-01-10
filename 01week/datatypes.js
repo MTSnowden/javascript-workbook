710936b6acc13f8114764e83f9c3a5e61a61c371
Michael Snowden <mtsnowden@gmail.com>
2019-01-10 17:29:59 -0600

    final

---
+++ 01week/datatypes.js
@@ -0,0 +1,47 @@
+// #1 program to display the current day and time
+let now = Date();
+now;
+
+// #2 program to convert a number to a string
+let num = 3939;
+let n = num.toString();
+n;
+
+// #3 program to convert a string to the number
+let string = "12345"
+parseInt(string);
+
+// #4:
+// boolean
+let michael = true;
+typeof(michael);
+
+// null
+let snowden = null;
+console.log(snowden);
+
+// undefined 
+let x;
+
+// number
+let RuPaul = 1000000;
+typeof(RuPaul);
+
+// NaN
+// is returning undefined 
+let billy = NaN;
+console.log(billy);
+
+// string
+let baseball = 'sport';
+typeof(baseball);
+
+// #5 program that adds 2 numbers together
+// let x = 5 + 8;
+// x;
+
+// #6 program that runs only when 2 things are true
+
+// #7 program that runs when 1 of 2 things are true
+
+// #8 program that runs when both things are not true
